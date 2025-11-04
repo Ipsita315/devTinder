c2e3193f192346595997cc40b1e7c8ed1868562b
Ipsita315 <ipsitarakshit315@gmail.com>
2025-11-04 10:59:45 +0530

    Exploring HTTP methods: GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -2,8 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-    res.send("Listening... yoyo ipsita")
+app.use(express.json());  //middleware
+
+// app.use("/user", (req, res) => {
+//     res.send("HAHAHA !!!!")
+// });
+
+app.get("/user", (req, res) => {
+    res.send({
+        firstName: "Ipsita",
+        lastName: "Rakshit"
+    })
+});
+
+app.post("/user", (req, res) => {
+    console.log("Req: ", req.body);
+    res.send("Data successfully saved to DB");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Data deleted successfully !!!")
+})
+
+app.use("/test", (req, res) => {
+    res.send("Test")
 });
 
 app.listen(3000, () => {
